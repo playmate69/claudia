17758d733af0c16b34ce2f4472cde14c97998966
playmate69 <kfchotwings@live.nl>
2019-11-02 01:39:05 +0000

    which

--- js/app.js
+++ js/app.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
 
 console.log('App loaded');
 
-$(document).on('click', '.step-btn', function() {
-    process($(this));
-})
+var view;
+$(document).on('scroll',  $(window), function() {
+    view = $(window).scrollTop();
+    $('.main-content').css('transform', 'translateY(-'+ view +'px)');
+});
 
-function process(el) {
-            var activeStep = $('.step-unit.active');
-    if(el.hasClass('prev')) {
-        activeStep.prev().addClass('active');
-        activeStep.removeClass('active');
-    } else {
-        activeStep.next().addClass('active');
-        activeStep.removeClass('active');
-    }
-}
+
 
 });
